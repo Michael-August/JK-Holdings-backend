60f01b62218ce1aeda7e0f7f36b67ee410f68da4
Michael-August <michaelfrancis135@gmail.com>
2023-01-05 06:46:06 +0100

    Project created

---
+++ src/main/java/com/jkholdings/jkholdings/JkholdingsApplication.java
@@ -0,0 +1,13 @@
+package com.jkholdings.jkholdings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkholdingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkholdingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkholdings/jkholdings/JkholdingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkholdings.jkholdings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkholdingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
